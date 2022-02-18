c923d26561925ca92f67ea8510f05232263f6c5b
Angel Lopez <angellopez15luisa@gmail.com>
2022-02-18 17:23:59 -0500

    Custom Hook useForm

---
+++ src/hooks/useForm.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+
+const useForm = (initialState={}) => {
+   
+    const [values, setValues] = useState(initialState);
+
+    const handleInputChange = (e) => {
+        
+        setValues({
+            ...values,
+            [e.target.name]: e.target.value
+        });
+    }
+
+    return [values,handleInputChange];
+
+}
+
+export default useForm
