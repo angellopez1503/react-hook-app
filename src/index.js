b1f7b9e45cd82a52f87f07dc418d294dcb9d35a7
Angel Lopez <angellopez15luisa@gmail.com>
2022-02-19 03:02:03 -0500

    Memo - Metodo de React

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import CounterApp from './components/01-useState/CounterApp';
-import CounterWithCustomHook from './components/01-useState/CounterWithCustomHook';
-import FormWithCustomHook from './components/02-useEffect/FormWithCustomHook';
-import SimpleForm from './components/02-useEffect/SimpleForm';
-import MultipleCustomHooks from './components/03-examples/MultipleCustomHooks';
-import FocusScreen from './components/04-useRef/FocusScreen';
-import RealExampleRef from './components/04-useRef/RealExampleRef';
-import Layout from './components/05-useLayoutEffect/Layout';
-import HookApp from './HookApp';
+// import CounterApp from './components/01-useState/CounterApp';
+// import CounterWithCustomHook from './components/01-useState/CounterWithCustomHook';
+// import FormWithCustomHook from './components/02-useEffect/FormWithCustomHook';
+// import SimpleForm from './components/02-useEffect/SimpleForm';
+// import MultipleCustomHooks from './components/03-examples/MultipleCustomHooks';
+// import FocusScreen from './components/04-useRef/FocusScreen';
+// import RealExampleRef from './components/04-useRef/RealExampleRef';
+// import Layout from './components/05-useLayoutEffect/Layout';
+import Memorize from './components/06-memos/Memorize';
+// import HookApp from './HookApp';
 
 
 ReactDOM.render(
@@ -23,7 +24,8 @@ ReactDOM.render(
   // <MultipleCustomHooks />
   // <FocusScreen />
   // <RealExampleRef />
-  <Layout />
+  // <Layout />
+  <Memorize />
   ,
 
   document.getElementById('root')
