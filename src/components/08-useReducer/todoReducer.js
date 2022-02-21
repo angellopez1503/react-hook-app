09789940f18b040559f7bed9cdb2c353eeb3e405
Angel Lopez <angellopez15luisa@gmail.com>
2022-02-21 15:32:26 -0500

    Borrar un TODO

--- src/components/08-useReducer/todoReducer.js
+++ src/components/08-useReducer/todoReducer.js
@@ -5,6 +5,10 @@ const todoReducer = (state=[],action) => {
         case 'add':
             
             return [...state, action.payload];
+        
+        case 'delete':
+
+            return state.filter(todo => todo.id !== action.payload);
 
         //     break;
         
