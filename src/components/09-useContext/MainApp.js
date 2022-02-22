ae112591ab57c8dc65dc7bf6e465840ad48e89a5
Angel Lopez <angellopez15luisa@gmail.com>
2022-02-21 23:33:29 -0500

    CreateContext y useContext

--- src/components/09-useContext/MainApp.js
+++ src/components/09-useContext/MainApp.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import AppRouter from './AppRouter'
+import UserContext from './UserContext'
+
 
 const MainApp = () => {
+
+  const user = {
+    id: 1234,
+    name: 'Angel',
+    email:'angel@gmail.com'
+  }
   return (
-   
+    <UserContext.Provider value={user}>
       <AppRouter />
-    
+    </UserContext.Provider>
+
   )
 }
 
