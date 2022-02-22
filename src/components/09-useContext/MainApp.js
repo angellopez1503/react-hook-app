375171144ab7f0a2f77fd1de3346a2b247bd7e6e
Angel Lopez <angellopez15luisa@gmail.com>
2022-02-21 19:25:56 -0500

    Configurar Router en React

--- src/components/09-useContext/MainApp.js
+++ src/components/09-useContext/MainApp.js
@@ -1,11 +1,11 @@
 import React from 'react'
+import AppRouter from './AppRouter'
 
 const MainApp = () => {
   return (
-      <div>
-          <h1>MainApp</h1>
-          <hr />
-    </div>
+   
+      <AppRouter />
+    
   )
 }
 
